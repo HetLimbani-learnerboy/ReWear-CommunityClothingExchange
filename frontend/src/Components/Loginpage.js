5257c61774ec64d5b0b9d17f2270296d389339ec
HetLimbani-learnerboy <hetlimbai61@gmail.com>
2025-07-12 10:13:26 +0530

    Pageinsert

---
+++ frontend/src/Components/Loginpage.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Loginpage=()=>{
+    return(
+        <div className="Loginpage-desgin"><h1>It is Loginpage</h1></div>
+    )
+}
+export default Loginpage;
---
+++ frontend/src/Components/Signuppage.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Signuppage=()=>{
+    return(
+        <div className="Signuppage-desgin"><h1>It is Signuppage</h1></div>
+    )
+}
+export default Signuppage;
