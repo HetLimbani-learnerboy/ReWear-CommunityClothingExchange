721814ffb60011a40860dbe99bd20beb9c074062
HetLimbani-learnerboy <hetlimbai61@gmail.com>
2025-07-12 10:03:17 +0530

    Homepage

---
+++ frontend/src/Components/Homepage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Homepage=()=>{
+    return(
+        <div className="Homepage-design">
+            <h1>Hello</h1>
+        </div>
+    )
+
+}
+export default Homepage;
