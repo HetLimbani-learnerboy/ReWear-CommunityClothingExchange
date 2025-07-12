938066a06115cf027d12113e3f19e5690a269ca2
HetLimbani-learnerboy <hetlimbai61@gmail.com>
2025-07-12 11:07:50 +0530

    pageoperate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./Components/Homepage";
+import Signuppage from "./Components/Signuppage";
+import Loginpage from "./Components/Loginpage";
+import Mainpage from "./Components/Mainpage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Homepage />} />
+        <Route path="/mainpage" element={<Mainpage/>}/>
+        <Route path="/signinpage" element={<Loginpage/>}/>
+        <Route path="/signuppage" element={<Signuppage/>}/>
+
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/Components/Homepage.js
+++ frontend/src/Components/Homepage.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Navigate, useNavigate } from "react-router-dom";
 
 const Homepage=()=>{
+    const navigate=useNavigate();
     return(
         <div className="Homepage-design">
             <h1>Hello</h1>
+            <button onClick={()=>navigate('/signuppage')} >Signuppage</button>
+            <button onClick={()=>navigate('/signinpage')} >Signin pagde</button>
+            <button onClick={()=>navigate('/mainpage')} >Get Started</button>
         </div>
     )
 
